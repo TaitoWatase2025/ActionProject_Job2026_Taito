295136a1423f2d1d149d890617e378f0fcb1b0b2
渡瀬　泰斗 <uni202506_14@telework.to>
2025-12-10 22:49:15 +0900

    修正
    初期音量調整

--- Assets/Scripts/UIAudioContoroller.cs
+++ Assets/Scripts/UIAudioContoroller.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class UIAudioController : MonoBehaviour
 {
+
     [Header("汎用AudioSource")]
     [SerializeField] private AudioSource audioSource;
 
     [Header("オーディオクリップ")]
     [SerializeField] private AudioClip hoverClip;
     [SerializeField] private AudioClip clickClip;
+
 
     public void PlayClickSE()
     {
